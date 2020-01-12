c56840fb56eac02881531dd442eee21bd6c6b3a7
Daniel Doblado <danieldoblado@gmail.com>
2020-01-12 19:53:07 +0100

    fix: Add hack to use gh-pages

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import App from './components/App.svelte'
 
+// Redirect for gh-pages
+// https://www.smashingmagazine.com/2016/08/sghpa-single-page-app-hack-github-pages/
+(() => {
+  const redirect = sessionStorage.redirect
+  delete sessionStorage.redirect
+  if (redirect && redirect !== location.href) {
+    history.replaceState(null, null, redirect)
+  }
+})()
+
 const app = new App({
   target: document.body
 })
